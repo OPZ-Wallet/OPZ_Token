783289475857da1e21f355612a550bcec15a4085
OPZ <151683640+OPZ-Wallet@users.noreply.github.com>
2024-04-27 13:38:22 +0400

    Update OPZToken.sol

--- OPZToken.sol
+++ OPZToken.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract OPZToken is ERC20, ERC20Permit {
-    constructor() ERC20("OPZ Token", "OPZ") ERC20Permit("OPZ Token") {
+contract OPZToken is ERC20 {
+    constructor() ERC20("OPZ Token", "OPZ") {
         _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
 }
